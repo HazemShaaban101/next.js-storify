a4ab10e8b6e91798afeaba78ad53f0b80da8e993
HazemShaaban101 <7azemsh3baan@gmail.com>
2025-08-28 09:20:59 +0300

    Add main slider for home screen

---
+++ src/app/_Components/MainHomeSlider/MainHomeSlider.tsx
@@ -0,0 +1,66 @@
+"use client";
+import Image from "next/image";
+import React, { useEffect } from "react";
+import image1 from "../../../../public/banner-4.jpeg";
+import image2 from "../../../../public/slider-image-2.jpeg";
+import image3 from "../../../../public/slider-image-3.jpeg";
+import image4 from "../../../../public/grocery-banner.png";
+import image5 from "../../../../public/grocery-banner-2.jpeg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay, Virtual } from "swiper/modules";
+
+export default function MainHomeSlider() {
+	useEffect(() => {
+		console.log("hello there");
+	});
+	return (
+		<>
+			<div className="flex w-[80%] mx-auto">
+				<div className="flex w-3/4">
+					<Swiper
+						spaceBetween={0}
+						slidesPerView={1}
+						modules={[Autoplay]}
+						autoplay={{ pauseOnMouseEnter: true, delay: 5000 }}
+						// onSwiper={(swiper) => swiper.update()}
+					>
+						<SwiperSlide className=" box-border">
+							<Image
+								src={image1}
+								alt="greens"
+								className=" h-[400px] object-cover object-center"
+							/>
+						</SwiperSlide>
+						<SwiperSlide className=" box-border">
+							<Image
+								src={image4}
+								alt="greens"
+								className=" h-[400px] object-cover object-center"
+							/>
+						</SwiperSlide>
+						<SwiperSlide className=" box-border">
+							<Image
+								src={image5}
+								alt="greens"
+								className=" h-[400px] object-cover object-center"
+							/>
+						</SwiperSlide>
+					</Swiper>
+				</div>
+				<div className=" flex flex-col object-cover">
+					<Image
+						src={image2}
+						alt="choco"
+						className="w-full h-[200px]"
+					/>
+					<Image
+						src={image3}
+						alt="food"
+						className="w-full h-[200px]"
+					/>
+				</div>
+			</div>
+		</>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import MainHomeSlider from "./_Components/MainHomeSlider/MainHomeSlider";
 
 export default function Home() {
 	return (
 		<>
 			<h1 className="text-green-400">Home</h1>
+			<MainHomeSlider />
 		</>
 	);
 }
