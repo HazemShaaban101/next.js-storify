36bd484aac7fe275637f02e33a68a279cd9adf59
HazemShaaban101 <7azemsh3baan@gmail.com>
2025-09-12 16:35:51 +0300

    Fix problems caused by pagination on products

--- src/apis/AllProducts.api.tsx
+++ src/apis/AllProducts.api.tsx
@@ -1,6 +1,8 @@
 import { ReadonlyURLSearchParams } from "next/navigation";
 
-export default async function AllProducts(page: ReadonlyURLSearchParams) {
+export default async function AllProducts(
+	page: ReadonlyURLSearchParams = new ReadonlyURLSearchParams("page=1")
+) {
 	let response = await fetch(
 		`https://ecommerce.routemisr.com/api/v1/products?limit=30&${page}`
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import MainHomeSlider from "./_Components/MainHomeSlider/MainHomeSlider";
 import CategorySlider from "./_Components/CategorySlider/CategorySlider";
 import { productType } from "./_interfaces/product.interface";
@@ -6,7 +5,7 @@ import AllProducts from "../apis/AllProducts.api";
 import { ProductCard } from "./_Components/ProductCard/ProductCard";
 
 export default async function Home() {
-	const data: productType[] = await AllProducts();
+	const data: { data: productType[] } = await AllProducts();
 	return (
 		<>
 			<div className="flex flex-col w-[80%] mx-auto gap-5 mb-10 overflow-hidden">
@@ -16,7 +15,7 @@ export default async function Home() {
 				<CategorySlider />
 			</div>
 			<div className=" w-full products grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-3">
-				{data.map((product: productType) => (
+				{data?.data.map((product: productType) => (
 					<ProductCard product={product} key={product._id} />
 				))}
 			</div>
